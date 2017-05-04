a30c33d0a5a4058e333ba5a6d0ec09562fd4aa77
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-05-04 17:04:05 -0400

    add respond_to blocks for html and json

--- sw-rails/app/controllers/films_controller.rb
+++ sw-rails/app/controllers/films_controller.rb
@@ -1,4 +1,34 @@
 class FilmsController < ApplicationController
   def index
+    @films = Film.all
+    @films = Film.new
+  end
+
+  def create
+    @film = Film.new(film_params)
+
+    respond_to do |format|
+      format.html do
+        if @film.save
+          redirect_to films_path
+        else
+          render :index
+        end
+      end
+
+      format.json do
+        if @film.save
+          render json: @film
+        else
+          render :index
+          # can also render json error message
+        end
+      end
+    end
+  end
+
+  private
+  def films_params
+    params.require(:film).permit(:title)
   end
 end
--- sw-rails/config/routes.rb
+++ sw-rails/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # get 'films/'
   # This is the same thing essentially
-  resources :films, only: [:index]
+  resources :films, only: [:create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
