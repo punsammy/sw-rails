4de263ded12cb6fc3ea3cdcfb0b24a3377348342
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-05-04 16:51:42 -0400

    add controller for Films

--- sw-rails/config/routes.rb
+++ sw-rails/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  # get 'films/'
+  # This is the same thing essentially
+  resources :films, only: [:index]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
